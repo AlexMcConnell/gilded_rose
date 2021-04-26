e319302ab9c7d028e4617d80a28bf93ef8a54f0e
Alex McConnell <alex.mcconnell@7factor.io>
2021-04-26 12:06:46 -0400

    Filling in missing ruby tests.

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -12,12 +12,36 @@ RSpec.describe GildedRose do
       expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 9)
     end
 
+    it "with min quality" do
+      gilded_rose = GildedRose.new(name: "foo", days_remaining: 5, quality: 0)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 0)
+    end
+
     it "on sell date" do
       gilded_rose = GildedRose.new(name: "foo", days_remaining: 0, quality: 10)
 
       gilded_rose.tick
 
       expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 8)
+    end
+
+    it "on sell date with min quality" do
+      gilded_rose = GildedRose.new(name: "foo", days_remaining: 0, quality: 0)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
+    end
+
+    it "on sell date near min quality" do
+      gilded_rose = GildedRose.new(name: "foo", days_remaining: 0, quality: 1)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
     end
 
     it "after sell date" do
@@ -28,12 +52,20 @@ RSpec.describe GildedRose do
       expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 8)
     end
 
-    it "of zero quality" do
-      gilded_rose = GildedRose.new(name: "foo", days_remaining: 5, quality: 0)
+    it "after sell date with min quality" do
+      gilded_rose = GildedRose.new(name: "foo", days_remaining: 0, quality: 0)
 
       gilded_rose.tick
 
-      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 0)
+      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
+    end
+
+    it "after sell date near min quality" do
+      gilded_rose = GildedRose.new(name: "foo", days_remaining: 0, quality: 1)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 0)
     end
   end
 
@@ -62,16 +94,16 @@ RSpec.describe GildedRose do
       expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 12)
     end
 
-    it "on sell date near max quality" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 49)
+    it "on sell date with max quality" do
+      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 50)
 
       gilded_rose.tick
 
       expect(gilded_rose).to have_attributes(days_remaining: -1, quality: 50)
     end
 
-    it "on sell date with max quality" do
-      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 50)
+    it "on sell date near max quality" do
+      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: 0, quality: 49)
 
       gilded_rose.tick
 
@@ -88,6 +120,14 @@ RSpec.describe GildedRose do
 
     it "after sell date with max quality" do
       gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 50)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: -11, quality: 50)
+    end
+
+    it "after sell date near max quality" do
+      gilded_rose = GildedRose.new(name: "Aged Brie", days_remaining: -10, quality: 49)
 
       gilded_rose.tick
 
@@ -154,6 +194,14 @@ RSpec.describe GildedRose do
       expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 50)
     end
 
+    it "medium close to sell date upper bound near max quality" do
+      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 49)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: 9, quality: 50)
+    end
+
     it "medium close to sell date lower bound" do
       gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
 
@@ -164,6 +212,14 @@ RSpec.describe GildedRose do
 
     it "medium close to sell date lower bound at max quality" do
       gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: 5, quality: 50)
+    end
+
+    it "medium close to sell date lower bound near max quality" do
+      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 49)
 
       gilded_rose.tick
 
@@ -186,6 +242,14 @@ RSpec.describe GildedRose do
       expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 50)
     end
 
+    it "very close to sell date upper bound near max quality" do
+      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 48)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: 4, quality: 50)
+    end
+
     it "very close to sell date lower bound" do
       gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
 
@@ -196,6 +260,14 @@ RSpec.describe GildedRose do
 
     it "very close to sell date lower bound at max quality" do
       gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
+
+      gilded_rose.tick
+
+      expect(gilded_rose).to have_attributes(days_remaining: 0, quality: 50)
+    end
+
+    it "very close to sell date lower bound near max quality" do
+      gilded_rose = GildedRose.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 48)
 
       gilded_rose.tick
 
