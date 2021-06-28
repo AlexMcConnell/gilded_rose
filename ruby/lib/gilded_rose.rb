dccf2af6d45c8442c11e0fc3828d0cd587dd0a01
Alex McConnell <alex.mcconnell@7factor.io>
2021-06-28 09:32:46 -0400

    Switching ruby back to using a list.

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,5 +1,9 @@
 class GildedRose
-  def process_end_of_day(item)
+  def process_end_of_day(items)
+    items.each { |item| process_item_end_of_day item }
+  end
+
+  def process_item_end_of_day(item)
     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.quality > 0
         if item.name != "Sulfuras, Hand of Ragnaros"
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -6,289 +6,289 @@ require_relative "../lib/item"
 RSpec.describe GildedRose do
   context "Normal Item" do
     it "before sell date" do
-      item = Item.new(name: "foo", days_remaining: 5, quality: 10)
+      items = [Item.new(name: "foo", days_remaining: 5, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 9)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 9)
     end
 
     it "with min quality" do
-      item = Item.new(name: "foo", days_remaining: 5, quality: 0)
+      items = [Item.new(name: "foo", days_remaining: 5, quality: 0)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 0)
     end
 
     it "on sell date" do
-      item = Item.new(name: "foo", days_remaining: 0, quality: 10)
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 8)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 8)
     end
 
     it "on sell date with min quality" do
-      item = Item.new(name: "foo", days_remaining: 0, quality: 0)
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 0)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
     end
 
     it "on sell date near min quality" do
-      item = Item.new(name: "foo", days_remaining: 0, quality: 1)
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 1)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
     end
 
     it "after sell date" do
-      item = Item.new(name: "foo", days_remaining: -10, quality: 10)
+      items = [Item.new(name: "foo", days_remaining: -10, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -11, quality: 8)
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 8)
     end
 
     it "after sell date with min quality" do
-      item = Item.new(name: "foo", days_remaining: 0, quality: 0)
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 0)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
     end
 
     it "after sell date near min quality" do
-      item = Item.new(name: "foo", days_remaining: 0, quality: 1)
+      items = [Item.new(name: "foo", days_remaining: 0, quality: 1)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
     end
   end
 
   context "Aged Brie" do
     it "before sell date" do
-      item = Item.new(name: "Aged Brie", days_remaining: 5, quality: 10)
+      items = [Item.new(name: "Aged Brie", days_remaining: 5, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 11)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 11)
     end
 
     it "with max quality" do
-      item = Item.new(name: "Aged Brie", days_remaining: 5, quality: 50)
+      items = [Item.new(name: "Aged Brie", days_remaining: 5, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 50)
     end
 
     it "on sell date" do
-      item = Item.new(name: "Aged Brie", days_remaining: 0, quality: 10)
+      items = [Item.new(name: "Aged Brie", days_remaining: 0, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 12)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 12)
     end
 
     it "on sell date with max quality" do
-      item = Item.new(name: "Aged Brie", days_remaining: 0, quality: 50)
+      items = [Item.new(name: "Aged Brie", days_remaining: 0, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 50)
     end
 
     it "on sell date near max quality" do
-      item = Item.new(name: "Aged Brie", days_remaining: 0, quality: 49)
+      items = [Item.new(name: "Aged Brie", days_remaining: 0, quality: 49)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 50)
     end
 
     it "after sell date" do
-      item = Item.new(name: "Aged Brie", days_remaining: -10, quality: 10)
+      items = [Item.new(name: "Aged Brie", days_remaining: -10, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -11, quality: 12)
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 12)
     end
 
     it "after sell date with max quality" do
-      item = Item.new(name: "Aged Brie", days_remaining: -10, quality: 50)
+      items = [Item.new(name: "Aged Brie", days_remaining: -10, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -11, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 50)
     end
 
     it "after sell date near max quality" do
-      item = Item.new(name: "Aged Brie", days_remaining: -10, quality: 49)
+      items = [Item.new(name: "Aged Brie", days_remaining: -10, quality: 49)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -11, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 50)
     end
   end
 
   context "Sulfuras" do
     it "before sell date" do
-      item = Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)
+      items = [Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 5, quality: 80)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 5, quality: 80)
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 80)
     end
 
     it "on sell date" do
-      item = Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)
+      items = [Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: 0, quality: 80)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 0, quality: 80)
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 80)
     end
 
     it "after sell date" do
-      item = Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)
+      items = [Item.new(name: "Sulfuras, Hand of Ragnaros", days_remaining: -10, quality: 80)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -10, quality: 80)
+      expect(items[0]).to have_attributes(days_remaining: -10, quality: 80)
     end
   end
 
   context "Backstage Pass" do
     it "long before sell date" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 10, quality: 11)
+      expect(items[0]).to have_attributes(days_remaining: 10, quality: 11)
     end
 
     it "long before sell date at max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 11, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 10, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 10, quality: 50)
     end
 
     it "medium close to sell date upper bound" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 9, quality: 12)
+      expect(items[0]).to have_attributes(days_remaining: 9, quality: 12)
     end
 
     it "medium close to sell date upper bound at max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 9, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 9, quality: 50)
     end
 
     it "medium close to sell date upper bound near max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 49)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 10, quality: 49)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 9, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 9, quality: 50)
     end
 
     it "medium close to sell date lower bound" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 5, quality: 12)
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 12)
     end
 
     it "medium close to sell date lower bound at max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 5, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 50)
     end
 
     it "medium close to sell date lower bound near max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 49)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 6, quality: 49)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 5, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 5, quality: 50)
     end
 
     it "very close to sell date upper bound" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 13)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 13)
     end
 
     it "very close to sell date upper bound at max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 50)
     end
 
     it "very close to sell date upper bound near max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 48)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 5, quality: 48)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 4, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 4, quality: 50)
     end
 
     it "very close to sell date lower bound" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 0, quality: 13)
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 13)
     end
 
     it "very close to sell date lower bound at max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 50)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 0, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 50)
     end
 
     it "very close to sell date lower bound near max quality" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 48)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 1, quality: 48)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: 0, quality: 50)
+      expect(items[0]).to have_attributes(days_remaining: 0, quality: 50)
     end
 
     it "on sell date" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: 0, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -1, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: -1, quality: 0)
     end
 
     it "after sell date" do
-      item = Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)
+      items = [Item.new(name: "Backstage passes to a TAFKAL80ETC concert", days_remaining: -10, quality: 10)]
 
-      GildedRose.new.process_end_of_day(item)
+      GildedRose.new.process_end_of_day(items)
 
-      expect(item).to have_attributes(days_remaining: -11, quality: 0)
+      expect(items[0]).to have_attributes(days_remaining: -11, quality: 0)
     end
   end
 end
